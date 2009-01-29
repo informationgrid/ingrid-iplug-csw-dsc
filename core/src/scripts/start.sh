efb86baf430a1cad746896e2a05ec1cea82f8baa
jm <jm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2009-01-29 20:03:27 +0000

    - add current start script

--- core/src/scripts/start.sh
+++ core/src/scripts/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# 
+#
 ##
 # Environment Variables
 #
@@ -10,75 +10,156 @@
 #   INGRID_OPTS      addtional java runtime options
 #
 
-# resolve links - $0 may be a softlink
 THIS="$0"
-while [ -h "$THIS" ]; do
-  ls=`ls -ld "$THIS"`
-  link=`expr "$ls" : '.*-> \(.*\)$'`
-  if expr "$link" : '.*/.*' > /dev/null; then
-    THIS="$link"
-  else
-    THIS=`dirname "$THIS"`/"$link"
-  fi
-done
 
 # some directories
 THIS_DIR=`dirname "$THIS"`
 INGRID_HOME=`cd "$THIS_DIR" ; pwd`
+PID=$INGRID_HOME/ingrid.pid
 
-# first sync libs
-echo '... syncronize libs from repository'
-rsync -av --update --existing $INGRID_HOME/../repository/ $INGRID_HOME/lib/
+# functions
+stopIplug()
+{
+  echo "Try stopping ingrid component ($INGRID_HOME)..."
+  if [ -f $PID ]; then
+      procid=`cat $PID`
+      idcount=`ps -p $procid | wc -l`
+      if [ $idcount -eq 2 ]; then
+        echo stopping $command
+        kill `cat $PID`
+        echo "process ($procid) has been terminated."
+      else
+        echo "process is not running. Exit."
+        exit 1
+      fi
+    else
+      echo "process is not running. Exit."
+      exit 1
+    fi
+}
+
+stopNoExitIplug()
+{
+  echo "Try stopping ingrid component ($INGRID_HOME)..."
+  if [ -f $PID ]; then
+      procid=`cat $PID`
+      idcount=`ps -p $procid | wc -l`
+      if [ $idcount -eq 2 ]; then
+        echo stopping $command
+        kill `cat $PID`
+        echo "process ($procid) has been terminated."
+      else
+        echo "process is not running. Exit."
+      fi
+    else
+      echo "process is not running. Exit."
+    fi
+}
 
 
-# some Java parameters
-if [ "$INGRID_JAVA_HOME" != "" ]; then
-  #echo "run java in $INGRID_JAVA_HOME"
-  JAVA_HOME=$INGRID_JAVA_HOME
-fi
+startIplug()
+{
+  echo "Try starting ingrid component ($INGRID_HOME)..."
+  if [ -f $PID ]; then
+      procid=`cat $PID`
+      idcount=`ps -p $procid | wc -l`
+      if [ $idcount -eq 2 ]; then
+        echo plug running as process `cat $PID`.  Stop it first.
+        exit 1
+      fi
+  fi
   
-if [ "$JAVA_HOME" = "" ]; then
-  echo "Error: JAVA_HOME is not set."
+  if [ -d "$INGRID_HOME/../repository/" ]; then
+  	echo 'syncronize libs from repository...'
+	rsync -av --update --existing $INGRID_HOME/../repository/ $INGRID_HOME/lib/
+	echo 'finished syncronize.'
+  fi
+  
+  # some Java parameters
+  if [ "$INGRID_JAVA_HOME" != "" ]; then
+    #echo "run java in $INGRID_JAVA_HOME"
+    JAVA_HOME=$INGRID_JAVA_HOME
+  fi
+  
+  if [ "$JAVA_HOME" = "" ]; then
+    echo "Error: JAVA_HOME is not set."
+    exit 1
+  fi
+  
+  JAVA=$JAVA_HOME/bin/java
+  JAVA_HEAP_MAX=-Xmx128m
+  
+  # check envvars which might override default args
+  if [ "$INGRID_HEAPSIZE" != "" ]; then
+    JAVA_HEAP_MAX="-Xmx""$INGRID_HEAPSIZE""m"
+    echo "run with heapsize $JAVA_HEAP_MAX"
+  fi
+
+  # CLASSPATH initially contains $INGRID_CONF_DIR, or defaults to $INGRID_HOME/conf
+  CLASSPATH=${INGRID_CONF_DIR:=$INGRID_HOME/conf}
+  CLASSPATH=${CLASSPATH}:$JAVA_HOME/lib/tools.jar
+  CLASSPATH=${CLASSPATH}:${INGRID_HOME}
+  
+  # so that filenames w/ spaces are handled correctly in loops below
+  IFS=
+  # add libs to CLASSPATH
+  for f in $INGRID_HOME/lib/*.jar; do
+    CLASSPATH=${CLASSPATH}:$f;
+  done
+  # restore ordinary behaviour
+  unset IFS
+  
+  # cygwin path translation
+  if expr `uname` : 'CYGWIN*' > /dev/null; then
+    CLASSPATH=`cygpath -p -w "$CLASSPATH"`
+  fi
+
+  CLASS=de.ingrid.iplug.PlugServer
+  
+  # run it
+  exec nohup "$JAVA" $JAVA_HEAP_MAX $INGRID_OPTS -classpath "$CLASSPATH" $CLASS --descriptor conf/communication.properties --plugdescription conf/plugdescription.xml > console.log &
+  
+  echo "ingrid component ($INGRID_HOME) started."
+  echo $! > $PID
+}
+
+# make sure the current user has the privilege to execute that script
+INGRID_USER="ingrid"
+
+STARTING_USER=`whoami`
+if [ "$STARTING_USER" != "$INGRID_USER" ]; then
+  echo "you must be user '$INGRID_USER' to start that script!"
   exit 1
-fi
+fi 
 
-JAVA=$JAVA_HOME/bin/java
-JAVA_HEAP_MAX=-Xmx1000m 
-
-# check envvars which might override default args
-if [ "$INGRID_HEAPSIZE" != "" ]; then
-  #echo "run with heapsize $INGRID_HEAPSIZE"
-  JAVA_HEAP_MAX="-Xmx""$INGRID_HEAPSIZE""m"
-  #echo $JAVA_HEAP_MAX
-fi
-
-# CLASSPATH initially contains $INGRID_CONF_DIR, or defaults to $INGRID_HOME/conf
-CLASSPATH=${INGRID_CONF_DIR:=$INGRID_HOME/conf}
-CLASSPATH=${CLASSPATH}:$JAVA_HOME/lib/tools.jar
-CLASSPATH=${CLASSPATH}:${INGRID_HOME}
-
-
-# so that filenames w/ spaces are handled correctly in loops below
-IFS=
-
-
-# add libs to CLASSPATH
-for f in $INGRID_HOME/lib/*.jar; do
-  CLASSPATH=${CLASSPATH}:$f;
-done
-
-# restore ordinary behaviour
-unset IFS
-
-
-CLASS=de.ingrid.iplug.PlugServer
-
-
-# cygwin path translation
-if expr `uname` : 'CYGWIN*' > /dev/null; then
-  CLASSPATH=`cygpath -p -w "$CLASSPATH"`
-fi
-
-# run it
-#exec "$JAVA" $JAVA_HEAP_MAX $INGRID_OPTS -classpath "$CLASSPATH" $CLASS "$@"
-exec "$JAVA" $JAVA_HEAP_MAX $INGRID_OPTS -classpath "$CLASSPATH" $CLASS 8477 8478 localhost 11112
+case "$1" in
+  start)
+    startIplug
+    ;;
+  stop)
+    stopIplug
+    ;;
+  restart)
+    stopNoExitIplug
+    echo "sleep 3 sec ..."
+    sleep 3
+    startIplug
+    ;;
+  status)
+    if [ -f $PID ]; then
+      procid=`cat $PID`
+      idcount=`ps -p $procid | wc -l`
+      if [ $idcount -eq 2 ]; then
+        echo "process ($procid) is running."
+      else
+        echo "process is not running. Exit."
+      fi
+    else
+      echo "process is not running. Exit."
+    fi
+    ;;
+  *)
+    echo "Usage: $0 {start|stop|restart|status}"
+    exit 1
+    ;;
+esac
