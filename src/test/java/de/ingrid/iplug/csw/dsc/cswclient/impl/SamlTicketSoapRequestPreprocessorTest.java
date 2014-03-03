04ea5c59ac23e66fd4bd7c26049595ca54ed2201
jm <jm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2014-03-03 17:01:23 +0000

    - revert to AXIS Soap client, prepare for RE-RELEASE

--- src/test/java/de/ingrid/iplug/csw/dsc/cswclient/impl/SamlTicketSoapRequestPreprocessorTest.java
+++ src/test/java/de/ingrid/iplug/csw/dsc/cswclient/impl/SamlTicketSoapRequestPreprocessorTest.java
@@ -4,19 +4,19 @@
 package de.ingrid.iplug.csw.dsc.cswclient.impl;
 
 import java.io.IOException;
-import java.util.Iterator;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 
-import javax.xml.namespace.NamespaceContext;
-import javax.xml.soap.MessageFactory;
-import javax.xml.soap.SOAPException;
-import javax.xml.soap.SOAPMessage;
+import javax.xml.namespace.QName;
 
 import junit.framework.TestCase;
 
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-
-import de.ingrid.utils.xpath.XPathUtils;
+import org.apache.axiom.om.OMElement;
+import org.apache.axis2.AxisFault;
+import org.apache.axis2.client.ServiceClient;
+import org.apache.axis2.context.ConfigurationContext;
+import org.apache.axis2.context.ConfigurationContextFactory;
+import org.springframework.core.io.ClassPathResource;
 
 /**
  * @author joachim
@@ -35,38 +35,35 @@ public class SamlTicketSoapRequestPreprocessorTest extends TestCase {
      * @throws SecurityException
      * @throws IllegalAccessException
      * @throws IllegalArgumentException
-     * @throws SOAPException
      */
-    public void testProcess() throws IOException, SecurityException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException, SOAPException {
+    @SuppressWarnings("unchecked")
+    public void testProcess() throws AxisFault, IOException, SecurityException, NoSuchFieldException,
+            IllegalArgumentException, IllegalAccessException {
         SamlTicketSoapRequestPreprocessor p = new SamlTicketSoapRequestPreprocessor();
         DummySamlTicketProvider provider = new DummySamlTicketProvider();
         p.setSamlTicketProvider(provider);
 
         // set up the client
+        ConfigurationContext configContext = ConfigurationContextFactory
+                .createConfigurationContextFromFileSystem((new ClassPathResource("axis2.xml")).getURI().getPath());
+        ServiceClient serviceClient = new ServiceClient(configContext, null);
+        p
+                .setSoapHeaderTemplate("<tcExt:tcSecurity soapenv:role=\"http://www.conterra.de/service.csw#component::terraCatalog#catalog\" soapenv:mustUnderstand=\"false\" xmlns:tcExt=\"http://www.conterra.de/catalog/ext\" xmlns:soapenv=\"http://www.w3.org/2003/05/soap-envelope\">\n"
+                        + "            <tcExt:tcSecuredAction>\n"
+                        + "                <tcExt:action>service.csw::discovery.read</tcExt:action>\n"
+                        + "            </tcExt:tcSecuredAction>\n"
+                        + "            <tcExt:samlTicket>${SAML_TICKET}</tcExt:samlTicket>\n"
+                        + "        </tcExt:tcSecurity>");
 
-        MessageFactory messageFactory = MessageFactory.newInstance();
-        SOAPMessage soapMessage = messageFactory.createMessage();
+        serviceClient = p.process(serviceClient);
 
-        p.setSoapHeaderTemplate("<tcExt:tcSecurity soapenv:role=\"http://www.conterra.de/service.csw#component::terraCatalog#catalog\" soapenv:mustUnderstand=\"false\" xmlns:tcExt=\"http://www.conterra.de/catalog/ext\" xmlns:soapenv=\"http://www.w3.org/2003/05/soap-envelope\">\n"
-                + "            <tcExt:tcSecuredAction>\n"
-                + "                <tcExt:action>service.csw::discovery.read</tcExt:action>\n"
-                + "            </tcExt:tcSecuredAction>\n"
-                + "            <tcExt:samlTicket>${SAML_TICKET}</tcExt:samlTicket>\n" + "        </tcExt:tcSecurity>");
-
-        p.process(soapMessage);
-
-        NodeList nodes = soapMessage.getSOAPPart().getEnvelope().getHeader().getChildNodes();
-        XPathUtils xpath = new XPathUtils(new TCExtNamespaceContext());
-
-        for (int i = 0; i < nodes.getLength(); i++) {
-            Node n = nodes.item(i);
-            if (xpath.getString(n, "//tcExt:samlTicket").equals(DummySamlTicketProvider.DUMMY_RESULT)) {
-                assertEquals(true, true);
-                return;
-            }
-        }
-
-        fail("Not found: " + DummySamlTicketProvider.DUMMY_RESULT);
+        Field reqField = ServiceClient.class.getDeclaredField("headers");
+        reqField.setAccessible(true);
+        ArrayList<OMElement> headers = (ArrayList<OMElement>) reqField.get(serviceClient);
+        OMElement e = headers.get(0);
+        assertNotNull(e);
+        OMElement result = e.getFirstChildWithName(new QName("http://www.conterra.de/catalog/ext", "samlTicket"));
+        assertEquals(result.getText(), DummySamlTicketProvider.DUMMY_RESULT);
     }
 
     public class DummySamlTicketProvider extends SamlTicketProvider {
@@ -80,25 +77,6 @@ public class SamlTicketSoapRequestPreprocessorTest extends TestCase {
         public String get() {
             return DUMMY_RESULT;
 
-        }
-
-    }
-
-    public class TCExtNamespaceContext implements NamespaceContext {
-
-        @Override
-        public String getNamespaceURI(String prefix) {
-            return "http://www.conterra.de/catalog/ext";
-        }
-
-        @Override
-        public String getPrefix(String namespaceURI) {
-            return "tcExt";
-        }
-
-        @Override
-        public Iterator getPrefixes(String namespaceURI) {
-            return null;
         }
 
     }
