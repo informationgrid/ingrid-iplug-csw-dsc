43f43e449d49363ef1eb163a28b6b8c2bb0caf89
André Wallat <andre.wallat@wemove.com>
2019-03-26 11:46:14 +0100

    Fix internal search

--- src/main/java/de/ingrid/iplug/csw/dsc/CswDscSearchPlug.java
+++ src/main/java/de/ingrid/iplug/csw/dsc/CswDscSearchPlug.java
@@ -103,7 +103,7 @@ public class CswDscSearchPlug extends HeartBeatPlug implements IRecordLoader {
         if (elasticConfig.esCommunicationThroughIBus) {
 
             ClauseQuery cq = new ClauseQuery(true, false);
-            cq.addField(new FieldQuery(true, false, "iPlugId", elasticConfig.communicationProxyUrl));
+            cq.addField(new FieldQuery(true, false, "iPlugId", JettyStarter.baseConfig.communicationProxyUrl));
             query.addClause(cq);
             return this.iBusIndexManager.search(query, start, length);
         }
