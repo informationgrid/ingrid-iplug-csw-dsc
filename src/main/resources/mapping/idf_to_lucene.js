43d6e0298ad701e824d9d79b72277db779954e44
Toan Tran <khanh.toan.tran@wemove.com>
2023-03-31 12:23:35 +0200

    Revert "Try add multi summaries"
    This reverts commit 474bc7f96daa0a05d91368c637696a5eaf0b42db.

--- src/main/resources/mapping/idf_to_lucene.js
+++ src/main/resources/mapping/idf_to_lucene.js
@@ -85,10 +85,8 @@ var transformationDescriptions = [
         "tokenized":true,
         "xpath":"//gmd:fileIdentifier/gco:CharacterString"
     },
-    {   "execute":{
-            "funct":addSummary,
-            "params":[recordNode]
-        }
+    {   "indexField":"summary",
+        "xpath":"//gmd:identificationInfo//gmd:abstract/gco:CharacterString"
     },
     {   "indexField":"t01_object.info_note",
         "xpath":"//gmd:identificationInfo//gmd:purpose/gco:CharacterString"
@@ -1101,16 +1099,6 @@ function addObjectReferenceTo() {
           addToDoc("object_reference.special_name", objSpecialName || "", false);
       }
   }
-}
-
-function addSummary() {
-    var summaries = XPathUtils.getNodeList(recordNode, "//gmd:identificationInfo//gmd:abstract/gco:CharacterString");
-    if (hasValue(summaries)) {
-        for (i=0; i<summaries.getLength(); i++ ) {
-            var summary = summaries.item(i);
-            addToDoc("summary", XPathUtils.getString(summary, "."), false);
-        }
-    }
 }
 
 function addToDoc(field, content, tokenized, additionalTokenize) {
